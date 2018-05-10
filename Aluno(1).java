4afcde5a7ad98e7b29af866116103e8e38f4294a
Camilacat05 <37548708+Camilacat05@users.noreply.github.com>
2018-05-10 10:43:43 -0300

    Classe Academia e Aluno model.bean

---
+++ Aluno(1).java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.bean;
+
+/**
+ *
+ * @author Alita
+ */
+public class Aluno {
+    
+    private String nomeAluno;
+    private String GraduacaoAlu;
+    private String sexo;
+    private int idade;
+ 
+    
+
+    public String getNomeAluno() {
+        return nomeAluno;
+    }
+
+    public void setNomeAluno(String nomeAluno) {
+        this.nomeAluno = nomeAluno;
+    }
+
+    public String getGraduacaoAlu() {
+        return GraduacaoAlu;
+    }
+
+    public void setGraduacaoAlu(String GradAluno) {
+        this.GraduacaoAlu= GradAluno;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    
+    
+}
