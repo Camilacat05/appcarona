4b0dec485f19bab4290c3397e4aaf9c0bfbd92e3
Alita <alytakallyne@gmail.com>
2018-05-26 17:04:06 -0300

    Testes

---
+++ ConfedeJudo/test/br/test/SuiteTest.java
@@ -0,0 +1,47 @@
+package br.test;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import .*;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Alita
+ */
+public class SuiteTest {
+    
+    public SuiteTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
