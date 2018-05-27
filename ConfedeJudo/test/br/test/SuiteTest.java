78d31b2bd175bd9b85979182dea3ee47318da618
Alita <alytakallyne@gmail.com>
2018-05-27 17:49:17 -0300

    AtualizandoTests

--- ConfedeJudo/test/br/test/SuiteTest.java
+++ ConfedeJudo/test/br/test/SuiteTest.java
@@ -1,47 +1,22 @@
+
 package br.test;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-import .*;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import static org.junit.Assert.*;
 
 /**
  *
  * @author Alita
  */
-public class SuiteTest {
-    
-    public SuiteTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+ @RunWith(Suite.class)
+ @Suite.SuiteClasses({
+   TestCadastroAcademia.class,
+   TestCadastroAluno.class,
+   TestCadastroCampeonato.class,
+   TestCadastroInscricao.class
+})
+public class SuiteTest {
+       
 }
