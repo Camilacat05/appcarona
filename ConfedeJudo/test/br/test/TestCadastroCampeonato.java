8bbe34fa5060a3f513c39d031eee19ba3e192859
Alita <alytakallyne@gmail.com>
2018-06-18 23:36:34 -0300

    AtualizandoTestCampeonato

--- ConfedeJudo/test/br/test/TestCadastroCampeonato.java
+++ ConfedeJudo/test/br/test/TestCadastroCampeonato.java
@@ -11,7 +11,7 @@ import org.junit.Test;
 
 /**
  *
- * @author Luis Guilherme
+ * @author Alita
  */
 public class TestCadastroCampeonato {
     
@@ -30,39 +30,139 @@ public class TestCadastroCampeonato {
        camp = null;
    }
    @Test
-   public void testCastroCampeonatoValido(){
+   public void testCadastroCampeonatoValido(){
        cp.setNomeCamp("Combate");
        cp.setProfResp("Gilberto");
        cp.setLocalCamp("Avenida Grande");
        cp.setDataCamp(20180324);
-       cp.setHorario(00001000);
+       cp.setHorario(100000);
        
        assertTrue(camp.cadastrarCampeonato(cp));
    }
    
    @Test
-   public void testCastroCampeonatoInvalido(){
+   public void testCadastroCampeonatoInvalido(){
        assertFalse(camp.cadastrarCampeonato(cp));
    }
    
    public void testCampoVazioValido(){
-       assertFalse(camp.verificaCampoVazio(cp));
+       assertTrue(camp.verificaCampoVazio(cp));
    }
    
     @Test 
    public void testCampoVazioInvalido(){
+       cp.setNomeCamp("");
        assertTrue(camp.verificaCampoVazio(cp));
    }
+   
    @Test
-   public void testCampoNomeValido(){
-       cp.setNomeCamp("Judo nova fase");
-       assertTrue(camp.verificaNome(cp));
+   public void testCampoNomeNumericoValido(){
+       cp.setNomeCamp("12345");
+       assertTrue(camp.verificaNomeNumerico(cp));
    }
    
    @Test
-   public void testCampoNomeInvalido(){
+   public void testCampoNomeNumericoInvalido(){
        
-       assertFalse(camp.verificaNome(cp));
+       assertFalse(camp.verificaNomeNumerico(cp));
    }
-   
+    @Test
+   public void testverificaNomeCaracterEspecialValido(){
+       cp.setNomeCamp("@#$%&*");
+       assertTrue(camp.verificaNomeCaracterEspecial(cp));
+   }
+   @Test
+   public void testverificaNomeCaracterEspecialInvalido(){
+      assertFalse(camp.verificaNomeCaracterEspecial(cp));
+   }
+   @Test
+   public void testverificaNomeCaracterEspecialNumericoValido(){
+       cp.setNomeCamp("123$%&*");
+       assertTrue(camp.verificaNomeCaracterEspecialNumerico(cp));
+   }
+   @Test
+   public void testverificaNomeCaracterEspecialNumericoInvalido(){
+       assertFalse(camp.verificaNomeCaracterEspecialNumerico(cp));
+   }
+   @Test
+   public void testverificaNomeProfCaracterEspecialValido(){
+       cp.setProfResp("@#$%&*");
+       assertTrue(camp.verificaNomeProfCaracterEspecial(cp));
+   }
+   @Test
+   public void testverificaNomeProfCaracterEspecialInvalido(){
+       assertFalse(camp.verificaNomeProfCaracterEspecial(cp));
+   }
+   @Test
+   public void testverificaNomeProfNumericoValido(){
+       cp.setProfResp("12345");
+       assertTrue(camp.verificaNomeProfNumerico(cp));
+   }
+   @Test
+   public void testverificaNomeProfNumericoInvalido(){
+       assertFalse(camp.verificaNomeProfNumerico(cp));
+   }
+   @Test
+   public void testverificaNomeProfCaracterEspecialNumericoValido(){
+       cp.setProfResp("@#$123");
+       assertTrue(camp.verificaNomeProfCaracterEspecialNumerico(cp));
+   }
+   @Test
+   public void testverificaNomeProfCaracterEspecialNumericoInvalido(){
+       assertFalse(camp.verificaNomeProfCaracterEspecialNumerico(cp));
+   }
+   @Test
+   public void testverificaNomeLocalCampNumericoValido(){
+       cp.setLocalCamp("12345");
+       assertTrue(camp.verificaNomeLocalCampNumerico(cp));
+   }
+   @Test
+   public void testverificaNomeLocalCampNumericoInvalido(){
+       assertFalse(camp.verificaNomeLocalCampNumerico(cp));
+   }
+   @Test
+   public void testverificaNomeLocalCampCaracterEspecialValido(){
+       cp.setLocalCamp("@#$%&*");
+       assertTrue(camp.verificaNomeLocalCampCaracterEspecial(cp));
+   }
+   @Test
+   public void testverificaNomeLocalCampCaracterEspecialInvalido(){
+       assertFalse(camp.verificaNomeLocalCampCaracterEspecial(cp));
+   }
+   @Test
+   public void testverificaNomeLocalCampCaracterEspecialNumericoValido(){
+       cp.setLocalCamp("@#$123");
+       assertTrue(camp.verificaNomeLocalCampCaracterEspecialNumerico(cp));
+   }
+   @Test
+   public void testverificaNomeLocalCampCaracterEspecialNumericoInvalido(){
+       assertFalse(camp.verificaNomeLocalCampCaracterEspecialNumerico(cp));
+   }
+   @Test
+   public void testverificaNomeDataValido(){
+       cp.setDataCamp(0);
+       assertTrue(camp.verificaNomeData(cp));
+   }
+   @Test
+   public void testverificaNomeDataInvalido(){
+      assertFalse(camp.verificaNomeData(cp));
+   }
+   @Test
+   public void testverificaNomeHorarioMenorValido(){
+       cp.setHorario(-3);
+       assertTrue(camp.verificaNomeHorarioMenor(cp));
+   }
+   @Test
+   public void testverificaNomeHorarioMenorInvalido(){
+       assertFalse(camp.verificaNomeHorarioMenor(cp));
+   }
+   @Test
+   public void testverificaNomeHorarioMaiorValido(){
+       cp.setHorario(10);
+       assertTrue(camp.verificaNomeHorarioMaior(cp));
+   }
+   @Test
+   public void testverificaNomeHorarioMaiorInvalido(){
+      assertFalse(camp.verificaNomeHorarioMaior(cp)); 
+   }
 }
